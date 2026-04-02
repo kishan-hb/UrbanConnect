0857eebd7d5d8fbf3e5773f92181687ae8e9a535
Kishan-hb <kishanhb3961@gmail.com>
2026-04-02 03:56:11 -0700

    feat: add authentication and authorization middleware

---
+++ server/middleware/role.js
@@ -0,0 +1,32 @@
+const User = require('../models/user');
+
+const requireRole = (allowedRole) => {
+  return async (req, res, next) => {
+    try {
+      const clerkId = req.auth?.userId;
+
+      if (!clerkId) {
+        return res.status(401).json({ message: 'Unauthorized' });
+      }
+
+      const user = await User.findOne({ clerkId });
+
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+
+      if (user.role !== allowedRole) {
+        return res.status(403).json({ message: 'Forbidden: insufficient privileges' });
+      }
+
+      req.user = user;
+      next();
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  };
+};
+
+module.exports = {
+  requireRole
+};
