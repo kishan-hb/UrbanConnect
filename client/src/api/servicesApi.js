a512734eaa24ca9d8582b4f96c3df90303e8ac77
Kishan-hb <kishanhb3961@gmail.com>
2026-04-16 12:00:00 -0700

    feat: update services api helpers

--- client/src/api/servicesApi.js
+++ client/src/api/servicesApi.js
@@ -1,7 +1,20 @@
 import apiClient from './apiClient';
 
-export function getAllServices() {
-  return apiClient('/api/services');
+export function getAllServices(params = {}) {
+  const searchParams = new URLSearchParams();
+
+  if (params.search) {
+    searchParams.set('search', params.search);
+  }
+
+  if (params.zip) {
+    searchParams.set('zip', params.zip);
+  }
+
+  const query = searchParams.toString();
+  const path = query ? `/api/services?${query}` : '/api/services';
+
+  return apiClient(path);
 }
 
 export function getServiceById(id) {
